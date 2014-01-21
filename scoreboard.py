2233111a51e18972908e6c7641f1d0c9d997782f
Jason <init6.init6.me>
2014-01-21 16:31:06 -0600

    working
    working copy. just need to make it look pretty.

--- scoreboard.py
+++ scoreboard.py
@@ -8,49 +8,6 @@
 from datetime import date
 import sys, re, sqlite3, hashlib, json
 
-#Set to True during game to prevent non-sense to FlagDB.
-ctf = False
-
-def main():
-    
-    #initialize the database with tables
-    if not ctf:
-        createDB()
-    updateSB()
-    
-    answer = input( "add team? \n" )
-    if answer == 'y':
-        TeamName = input( "Enter TeamName \n" )
-        hTeamName = hashlib.sha1(TeamName.encode()).hexdigest()
-
-        status = addTeam(hTeamName, TeamName)
-
-        if status:
-            print ( " Team %s was added. \n" % TeamName )
-    else:
-        pass
-
-    answer = input( "enter flag y or n" )
-    if answer == 'y':
-        flag = input( "Flag: \n" )
-        points = input( "Points: \n" )
-        status = addFlag(flag, points)
-        if status:
-            print ( "Flag %s was added with %s points. \n" % (flag, points)) 
-    else:
-        pass
-
-    answer = input( "Update team y or n. \n" )
-    if answer == 'y':
-        TeamName = input( "Enter TeamName \n" )
-        hTeamName = hashlib.sha1(TeamName.encode()).hexdigest()
-        flag = input( "Enter Flag: \n" )
-
-        status = updateTeam(hTeamName, flag)
-        if status:
-            print ( "Team %s was updated. \n" % (TeamName) )
-        
-    
 def updateSB():
     try:
         json_db = {}
@@ -70,7 +27,7 @@ def updateSB():
             for hName in teams:
                 tableName = 'a'+hName
                 cur.execute("SELECT * FROM "+tableName)
-                teamRow = cur.fetchone() #hName TEXT, name TEXT, flagID INTEGER, flags TEXT, flagPoints INTEGER, tPoints INTEGER
+                teamRow = cur.fetchone()
                 team = {}
                 team['TeamHash'] = hName
                 team['TeamName'] = teamRow[1]
@@ -84,12 +41,10 @@ def updateSB():
             #sort hs. then go through list if hs equals teamPoints take index as place. 
             hs = sorted(hs, reverse=True)
             for k in json_db['Teams']:
-                k['Rank'] = int(hs.index(k['teamPoints'])) + 1 
-                        
-            #print( json.dumps(json_db, indent=4, separators=(',', ': ')) )
+                k['Rank'] = int(hs.index(k['teamPoints'])) + 1
+                
             return json_db
-    
-    
+        
     except sqlite3.Error as e:
         print ( "Error %s:" % e.args[0] )
         sys.exit(1)
@@ -119,8 +74,8 @@ def addTeam(hName, name):
                 status = "success"
                 return status
 
-            #Creates a list of all teams then it checks to see if hName is already in the database if not adds it. if so prints error msg.
-            #hName TEXT, name TEXT, flagID INTEGER, flags TEXT, flagPoints INTEGER, tPoints INTEGER
+            #Creates a list of all teams then it checks to see if hName is already in the database
+            #if not adds it. if so prints error msg.
             else:
                 for row in rows:
                     teams.append(row[0])
@@ -168,7 +123,8 @@ def updateTeam(hName, flag):
                 if not rows:
                     return status
 
-                #Creates a list of all teams then it checks to see if hName is in the database. if so, updates flag, if not, errors.
+                #Creates a list of all teams then it checks to see if hName is in the database.
+                #if so, updates flag, if not, errors.
                 else:
                     for row in rows:
                         teams.append(row[0])
@@ -190,8 +146,7 @@ def updateTeam(hName, flag):
                             teamPoints += points
                             #How many flags does the team have.
                             hFlags = int(teamFlags) + 1
-                     
-                            #hName TEXT, name TEXT, flagID INTEGER, flags TEXT, flagPoints INTEGER, tPoints INTEGER
+
                             cur.execute("UPDATE "+tableName+" SET flags=?,tPoints=? WHERE hName=?", (str(hFlags), teamPoints, hName)  )
                             cur.execute("INSERT INTO "+tableName+" VALUES (?,?,?,?,?,?)", (0, 0, flagID, flag, points, 0) )
                             status = "success"
@@ -217,54 +172,7 @@ def updateTeam(hName, flag):
         print ( "Flag not in database " )
         return status
 
-#comment out function for game
-def addFlag(flag, points):
-    if not ctf:
-        try:
-            flags = []
-            status = None
-            conn = sqlite3.connect('scoreboard.db')
 
-            with conn:
-                cur = conn.cursor()
-
-                cur.execute("SELECT max(id) FROM flags")
-                max_id = cur.fetchone()[0]
-
-                cur.execute("SELECT * FROM flags")
-                rows = cur.fetchall()
-                #if table is empty no flags have been added go ahead and add flag. 
-                if not rows:
-                    cur.execute("INSERT INTO flags VALUES (?, ?, ?)", (max_id, flag, points))
-                    status = "success"
-                    return status
-
-                #Creates a list of all flags then it checks to see if flag is already in the database if not adds it. if so prints error msg.
-                else:
-                    for row in rows:
-                        flags.append(row[1])
-                        
-                    if flag not in flags:
-                        max_id += 1
-                        cur.execute("INSERT INTO flags VALUES (?, ?, ?)", (max_id, flag, points))
-                        status = "success"
-                        return status
-
-                    else:
-                        print ( "Flag already in database" )
-                        status = "Fail"
-                        return status
-
-        except sqlite3.Error as e:
-            print ( "Error %s:" % e.args[0] )
-            sys.exit(1)
-
-        finally:
-            if conn:
-                conn.close()
-        
-    
-    
 def checkFlag(flag):
     try:
         flags = {}
