3fd65a9a5653878e2147152eb48139baa8b4fa8a
Jason <init6.init6.me>
2014-01-24 16:31:41 -0600

    started on sort

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-# Read in file.
+
 # reject anything with space or $%^&*()+{}[]|\<>,.?/
 # reject anything with the word rock in it.
 # reject anything less than 5 or greater than 14
@@ -10,5 +10,22 @@
 # mix up list.
 # write to file in format <hash><tab><points>
 
-import sys
+import sys, re
 
+fname = 'all.txt'
+clean_list = []
+
+
+# Read in file.
+
+with open(fname, 'r', encoding='ascii', errors='surrogateescape') as f:
+    data = f.readlines()
+
+for word in data:
+    clean_list.append(word.encode('ascii', 'replace') )
+    #print ( word.encode('ascii').strip() )
+    #if not re.search('rock', word): # and not re.search([\$%^&*()+{}[\]|\\<>,.?/ ]):
+        #print( word.encode("ascii", "ignore") )
+        #clean_list.append(word.encode(utf-8))
+
+print( clean_list )
